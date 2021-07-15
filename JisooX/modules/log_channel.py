f9d886ab9e200c22c864d93fd1a62ca19ea396dd
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-16 06:07:51 +0700

    Update log_channel.py

--- JisooX/modules/log_channel.py
+++ JisooX/modules/log_channel.py
@@ -183,7 +183,7 @@ if is_module_loaded(FILENAME):
     - sending /setlog in the channel
     - forwarding the /setlog to the group
     """ 
-    __mod_name__ = "LOG CHANNELS"
+    __mod_name__ = "LOG"
   
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
