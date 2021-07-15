ae54cf5968bb2fc61cf989677ddcd90139a1cd94
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-15 12:10:09 +0700

    Update blacklist_sql.py

--- JisooX/modules/sql/blacklist_sql.py
+++ JisooX/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText
 
-from cinderella.modules.sql import SESSION, BASE
+from JisooX.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
