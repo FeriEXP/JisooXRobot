8b4bedf890bc24b57a31f2ab34c3a94bc2030e8f
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-16 06:05:49 +0700

    Update lyrics.py

--- JisooX/modules/lyrics.py
+++ JisooX/modules/lyrics.py
@@ -32,19 +32,6 @@ def lyrics(bot: Bot, update: Update, args):
                 caption="Message length exceeded max limit! Sending as a text file.")
         else:
             msg.reply_text(reply)
-                
-        
-                
-__help__ = """
-Want to get the lyrics of your favorite songs straight from the app? This module is perfect for that!
-
-*Available commands:*
- - /lyrics <song>: returns the lyrics of that song.
- You can either enter just the song name or both the artist and song name.
-"""
-
-__mod_name__ = "SONG LYRICS"
-
 
 
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
