d5149bb9b4618070820c6da51ad91eb7308b84e0
Shin Ryujin <82243933+ferikunn@users.noreply.github.com>
2021-07-16 19:26:14 +0700

    update text to speech

--- JisooX/modules/tts.py
+++ JisooX/modules/tts.py
@@ -1,44 +1,108 @@
-from telegram import ChatAction
-from gtts import gTTS
-import html
+from JisooX import telethn as tbot
+import os
 import urllib.request
-import re
-import json
 from datetime import datetime
-from typing import Optional, List
-import time
+from typing import List
+from typing import Optional
 import requests
-from telegram import Message, Chat, Update, Bot, MessageEntity
-from telegram import ParseMode
-from telegram.ext import CommandHandler, run_async, Filters
-from telegram.utils.helpers import escape_markdown, mention_html
-from JisooX import dispatcher
-from JisooX.__main__ import STATS
-from JisooX.modules.disable import DisableAbleCommandHandler
-from JisooX.modules.helper_funcs.extraction import extract_user
+from telethon import *
+from telethon import events
+from telethon.tl import functions
+from telethon.tl import types
+from telethon.tl.types import *
 
-def tts(bot: Bot, update: Update, args):
-    current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
-    filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
-    reply = " ".join(args)
-    update.message.chat.send_action(ChatAction.RECORD_AUDIO)
-    lang="ml"
-    tts = gTTS(reply, lang)
-    tts.save("k.mp3")
-    with open("k.mp3", "rb") as f:
-        linelist = list(f)
-        linecount = len(linelist)
-    if linecount == 1:
-        update.message.chat.send_action(ChatAction.RECORD_AUDIO)
-        lang = "id"
-        tts = gTTS(reply, lang)
-        tts.save("k.mp3")
-    with open("k.mp3", "rb") as speech:
-        update.message.reply_voice(speech, quote=False)
+from JisooX import *
+from JisooX.events import register
 
-__help__ = """ Text to speech
-- /tts <your text>
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerUser):
+        return True
+
+
+@register(pattern="^/stt$")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_group:
+     if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+       await event.reply("🚨 Need Admin Pewer.. You can't use this command.. But you can use in my pm")
+       return
+
+    start = datetime.now()
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        required_file_name = await event.client.download_media(
+            previous_message, TEMP_DOWNLOAD_DIRECTORY
+        )
+        if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
+            await event.reply(
+                "You need to set the required ENV variables for this module. \nModule stopping"
+            )
+        else:
+            # await event.reply("Starting analysis")
+            headers = {
+                "Content-Type": previous_message.media.document.mime_type,
+            }
+            data = open(required_file_name, "rb").read()
+            response = requests.post(
+                IBM_WATSON_CRED_URL + "/v1/recognize",
+                headers=headers,
+                data=data,
+                auth=("apikey", IBM_WATSON_CRED_PASSWORD),
+            )
+            r = response.json()
+            if "results" in r:
+                # process the json to appropriate string format
+                results = r["results"]
+                transcript_response = ""
+                transcript_confidence = ""
+                for alternative in results:
+                    alternatives = alternative["alternatives"][0]
+                    transcript_response += " " + str(alternatives["transcript"])
+                    transcript_confidence += (
+                        " " + str(alternatives["confidence"]) + " + "
+                    )
+                end = datetime.now()
+                ms = (end - start).seconds
+                if transcript_response != "":
+                    string_to_show = "TRANSCRIPT: `{}`\nTime Taken: {} seconds\nConfidence: `{}`".format(
+                        transcript_response, ms, transcript_confidence
+                    )
+                else:
+                    string_to_show = "TRANSCRIPT: `Nil`\nTime Taken: {} seconds\n\n**No Results Found**".format(
+                        ms
+                    )
+                await event.reply(string_to_show)
+            else:
+                await event.reply(r["error"])
+            # now, remove the temporary file
+            os.remove(required_file_name)
+    else:
+        await event.reply("Reply to a voice message, to get the text out of it.")
+
+
+__help__ = """
+I can convert text to voice and voice to text..
+ ❍ /tts <lang code>*:* Reply to any message to get text to speech output
+ ❍ /stt*:* Type in reply to a voice message(support english only) to extract text from it.
+*Language Codes*
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
-__mod_name__ = "TTS"
 
-dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
+__mod_name__ = "TTS/STT"
