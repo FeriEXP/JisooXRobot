32c1816c315d702e1850d4fc8bbfd6eea4584c07
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-16 22:08:43 +0700

    Create repo.py

---
+++ JisooX/modules/repo.py
@@ -0,0 +1,33 @@
+from pyrogram import filters
+from JisooX import pbot as app
+from JisooX.utils.http import get
+
+
+@app.on_message(filters.command("repo") & ~filters.edited)
+async def repo(_, message):
+    users = await get(
+        "https://api.github.com/repos/ferikunn/JisooXRobot/contributors"
+    )
+    list_of_users = ""
+    count = 1
+    for user in users:
+        list_of_users += (
+            f"**{count}.** [{user['login']}]({user['html_url']})\n"
+        )
+        count += 1
+
+    text = f"""[Github](https://github.com/ferikunn/JisooXRobot) | [Group](https://t.me/AnosSupport)
+```----------------
+| Contributors |
+----------------```
+{list_of_users}"""
+    await app.send_message(
+        message.chat.id, text=text, disable_web_page_preview=True
+    )
+
+
+__help__ = """
+ ❍ /repo*:* To Get My Github Repository Link And Support Group Link
+"""
+
+__mod_name__ = "Repo"
