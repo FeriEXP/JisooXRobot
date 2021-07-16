dd08e918ad9086b9cc2c38e24ba23e66c3a8f24b
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-16 21:56:27 +0700

    Create http.py

---
+++ JisooX/utils/http.py
@@ -0,0 +1,55 @@
+from asyncio import gather
+from aiohttp import ClientSession
+tede = ClientSession()
+
+async def get(url: str, *args, **kwargs):
+    async with tede.get(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+
+async def head(url: str, *args, **kwargs):
+    async with tede.head(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+
+async def post(url: str, *args, **kwargs):
+    async with tede.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+
+async def multiget(url: str, times: int, *args, **kwargs):
+    return await gather(
+        *[get(url, *args, **kwargs) for _ in range(times)]
+    )
+
+
+async def multihead(url: str, times: int, *args, **kwargs):
+    return await gather(
+        *[head(url, *args, **kwargs) for _ in range(times)]
+    )
+
+
+async def multipost(url: str, times: int, *args, **kwargs):
+    return await gather(
+        *[post(url, *args, **kwargs) for _ in range(times)]
+    )
+
+
+async def resp_get(url: str, *args, **kwargs):
+    return await tede.get(url, *args, **kwargs)
+
+
+async def resp_post(url: str, *args, **kwargs):
+    return await tede.post(url, *args, **kwargs)
