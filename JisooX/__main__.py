fe9f37a1bc42aea3d604ba0737f12191919f83ca
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-07-27 09:45:12 +0700

    Update __main__.py

--- JisooX/__main__.py
+++ JisooX/__main__.py
@@ -180,7 +180,7 @@ def send_start(bot, update):
     text = PM_START_TEXT
 
     keyboard = [[InlineKeyboardButton(text="[► Help ◄]",callback_data="help_back"),InlineKeyboardButton(text="[► Creator ◄]",url="https://t.me/xflicks")]]
-    keyboard += [[InlineKeyboardButton(text="[► Anos ◄]", callback_data="t.me/AnosVoldigoadBot"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard += [[InlineKeyboardButton(text="[► Anos ◄]",url="https://t.me/AnosVoldigoadBot"),InlineKeyboardButton(text="[► Add Me ◄]",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
