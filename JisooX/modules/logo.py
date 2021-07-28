f253707ba052ee4f0ff125f13ab3c16080790471
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-07-28 11:37:22 +0700

    Update logo.py

--- JisooX/modules/logo.py
+++ JisooX/modules/logo.py
@@ -20,14 +20,14 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./JisooX/resources/Lucky-Boss.ttf", 50)
+    font = ImageFont.truetype("./JisooX/resources/Lucky-Boss.ttf", 55)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=6, stroke_fill="black")
+    draw.text((x, y), text, font=font, fill="black", stroke_width=4, stroke_fill="yellow")
     fname2 = "Jisoo.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Made By @JisooXRobot")
