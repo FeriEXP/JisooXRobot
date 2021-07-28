311aefda030b3ed72d6213e06541074655312583
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-28 11:59:48 +0700

    Create truth_and_dare.py

---
+++ JisooX/modules/truth_and_dare.py
@@ -0,0 +1,22 @@
+import requests
+from JisooX.events import register
+
+
+@register(pattern="^/truth ?(.*)")
+async def _(td):
+    try:
+        kuntul = requests.get("https://tede-api.herokuapp.com/api/truth").json()
+        results = f"{kuntul['message']}"
+        return await td.reply(results)
+    except Exception:
+        await td.reply("`Something went wrong LOL...`")
+
+
+@register(pattern="^/dare ?(.*)")
+async def _(dr):
+    try:
+        kuntul = requests.get("https://tede-api.herokuapp.com/api/dare").json()
+        results = f"{kuntul['message']}"
+        return await dr.reply(results)
+    except Exception:
+        await dr.reply("`Something went wrong LOL...`")
