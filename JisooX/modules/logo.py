37f6625d5e70033c29757e7029dab45660826484
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-07-28 10:58:08 +0700

    Update logo.py

--- JisooX/modules/logo.py
+++ JisooX/modules/logo.py
@@ -14,7 +14,7 @@ async def lego(event):
      
     if not xnxx:
       xnxx = await event.reply('Provide Some Text To Draw!')
-       return
+      
     try:
     await event.reply('Creating your logo...wait!')
     img = Image.open('./JisooX/resources/951b8baf6839dccee3bbd7ab16b23813.jpg')
