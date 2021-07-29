477b27845ba8a3e69c78b4f9c85c19161247ff38
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-07-29 10:06:03 +0700

    Update __init__.py

--- JisooX/__init__.py
+++ JisooX/__init__.py
@@ -65,6 +65,7 @@ if ENV:
     CERT_PATH = os.environ.get("CERT_PATH")
 
     DB_URI = os.environ.get('DATABASE_URL')
+    MONGO_DB_URI = Config.MONGO_DB_URI
     BOT_ID = int(os.environ.get("BOT_ID", None))
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
@@ -133,6 +134,7 @@ else:
 
     DB_URI = Config.SQLALCHEMY_DATABASE_URI
     BOT_ID = Config.BOT_ID
+    MONGO_DB_URI = Config.MONGO_DB_URI
     HEROKU_API_KEY = Config.HEROKU_API_KEY
     HEROKU_APP_NAME = Config.HEROKU_APP_NAME
     ARQ_API = Config.ARQ_API_KEY
