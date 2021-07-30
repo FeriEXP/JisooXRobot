23ef99579893c151a1c7593c47790fb361f755ad
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-07-30 08:17:17 +0700

    ngeue

--- JisooX/modules/asupan.py
+++ JisooX/modules/asupan.py
@@ -8,10 +8,9 @@ async def asupan(event):
     try:
         resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
         asupannya = f"{resp['url']}"
-        emrornya = f"{resp['error']['message']}"
         return await tbot.send_file(event.chat_id, asupannya)
     except Exception:
-        await event.reply(f"{emrornya}")
+        await event.reply("`Something went wrong LOL...`")
 
 
 @register(pattern="^/wibu ?(.*)")
@@ -19,7 +18,6 @@ async def wibu(event):
     try:
         resp = requests.get("https://tede-api.herokuapp.com/api/asupan/wibu").json()
         wibunya = f"{resp['url']}"
-        emrornya = f"{resp['error']['message']}"
         return await tbot.send_file(event.chat_id, wibunya)
     except Exception:
-        await event.reply(f"{emrornya}")
+        await event.reply("`Something went wrong LOL...`")
