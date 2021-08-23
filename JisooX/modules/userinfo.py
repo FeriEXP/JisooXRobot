c4b47a22d55122087b9a3d2827f06a5dab647895
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-08-23 17:38:28 +0700

    Update userinfo.py

--- JisooX/modules/userinfo.py
+++ JisooX/modules/userinfo.py
@@ -1,10 +1,8 @@
 import html
 from typing import List
-
 from telegram import Bot, Update, ParseMode, MAX_MESSAGE_LENGTH
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown
-
 import JisooX.modules.sql.userinfo_sql as sql
 from JisooX import dispatcher, SUDO_USERS, DEV_USERS
 from JisooX.modules.disable import DisableAbleCommandHandler
@@ -134,7 +132,6 @@ __help__ = """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
 GET_BIO_HANDLER = DisableAbleCommandHandler("bio", about_bio, pass_args=True)
-
 SET_ABOUT_HANDLER = DisableAbleCommandHandler("setme", set_about_me)
 GET_ABOUT_HANDLER = DisableAbleCommandHandler("me", about_me, pass_args=True)
 
