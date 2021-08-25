823d14e3b218760bb7baba28195651ef5e5f95f1
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-08-25 19:18:04 +0700

    Update truth_and_dare.py

--- JisooX/modules/truth_and_dare.py
+++ JisooX/modules/truth_and_dare.py
@@ -5,18 +5,18 @@ from JisooX.events import register
 @register(pattern="^/truth ?(.*)")
 async def _(td):
     try:
-        kuntul = requests.get("https://tede-api.herokuapp.com/api/truth").json()
+        kuntul = requests.get("https://api-tede.herokuapp.com/api/truth").json()
         results = f"**{kuntul['message']}**"
         return await td.reply(results)
     except Exception:
-        await td.reply("`Something went wrong LOL...`")
+        await td.reply("`Sedang Bermasalah...`")
 
 
 @register(pattern="^/dare ?(.*)")
 async def _(dr):
     try:
-        kuntul = requests.get("https://tede-api.herokuapp.com/api/dare").json()
+        kuntul = requests.get("https://api-tede.herokuapp.com/api/dare").json()
         results = f"**{kuntul['message']}**"
         return await dr.reply(results)
     except Exception:
-        await dr.reply("`Something went wrong LOL...`")
+        await dr.reply("`Sedang Bermasalah...`")
