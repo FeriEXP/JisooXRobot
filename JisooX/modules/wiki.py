21582cc9de7636e075ce181077b4a26ea5d06ceb
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-27 01:44:54 +0700

    Update wiki.py

--- JisooX/modules/wiki.py
+++ JisooX/modules/wiki.py
@@ -1,17 +1,17 @@
-import re
-import json
-import urllib.request
-import urllib.parse
 import wikipedia
-from wikipedia.exceptions import DisambiguationError, PageError
-from telegram import Message, Chat, Update, Bot, ParseMode
-from telegram.ext import run_async
-
 from JisooX import dispatcher
 from JisooX.modules.disable import DisableAbleCommandHandler
-@run_async
-def wiki(bot: Bot, update: Update):
-    msg = update.effective_message.reply_to_message if update.effective_message.reply_to_message else update.effective_message
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+from wikipedia.exceptions import DisambiguationError, PageError
+
+
+def wiki(update: Update, context: CallbackContext):
+    msg = (
+        update.effective_message.reply_to_message
+        if update.effective_message.reply_to_message
+        else update.effective_message
+    )
     res = ""
     if msg == update.effective_message:
         search = msg.text.split(" ", maxsplit=1)[1]
@@ -20,32 +20,36 @@ def wiki(bot: Bot, update: Update):
     try:
         res = wikipedia.summary(search)
     except DisambiguationError as e:
-        update.message.reply_text("Disambiguated pages found! Adjust your query accordingly.\n<i>{}</i>".format(e),
-        parse_mode=ParseMode.HTML)
+        update.message.reply_text(
+            "Disambiguated pages found! Adjust your query accordingly.\n<i>{}</i>".format(
+                e
+            ),
+            parse_mode=ParseMode.HTML,
+        )
     except PageError as e:
-        update.message.reply_text("<code>{}</code>".format(e), parse_mode=ParseMode.HTML)
+        update.message.reply_text(
+            "<code>{}</code>".format(e), parse_mode=ParseMode.HTML
+        )
     if res:
         result = f"<b>{search}</b>\n\n"
         result += f"<i>{res}</i>\n"
         result += f"""<a href="https://en.wikipedia.org/wiki/{search.replace(" ", "%20")}">Read more...</a>"""
         if len(result) > 4000:
-            with open("result.txt", 'w') as f:
+            with open("result.txt", "w") as f:
                 f.write(f"{result}\n\nUwU OwO OmO UmU")
-            with open("result.txt", 'rb') as f:
-                bot.send_document(document=f, filename=f.name,
-                    reply_to_message_id=update.message.message_id, chat_id=update.effective_chat.id,
-                    parse_mode=ParseMode.HTML)
+            with open("result.txt", "rb") as f:
+                context.bot.send_document(
+                    document=f,
+                    filename=f.name,
+                    reply_to_message_id=update.message.message_id,
+                    chat_id=update.effective_chat.id,
+                    parse_mode=ParseMode.HTML,
+                )
         else:
-            update.message.reply_text(result, parse_mode=ParseMode.HTML)
-__help__ = """
-WIKIPEDIA!!
+            update.message.reply_text(
+                result, parse_mode=ParseMode.HTML, disable_web_page_preview=True
+            )
 
-*Available commands:*
- - /wiki <query>: wiki your query.
 
-"""
-
-__mod_name__ = "WIKIPEDIA"
-
-WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
+WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, run_async=True)
 dispatcher.add_handler(WIKI_HANDLER)
