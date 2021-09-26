079d3397d5889a1501a4337b768441f22f6129e0
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-09-27 01:09:00 +0700

    Update time.py

--- JisooX/modules/time.py
+++ JisooX/modules/time.py
@@ -1,28 +1,29 @@
-import requests
-import json
 import datetime
-
 from typing import List
 
-from telegram import Bot, Update, ParseMode
-from telegram.ext import run_async
-
-from JisooX import dispatcher, TIME_API_KEY
+import requests
+from JisooX import TIME_API_KEY, dispatcher
 from JisooX.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext
+
 
 def generate_time(to_find: str, findtype: List[str]) -> str:
-
-    data = requests.get(f"http://api.timezonedb.com/v2.1/list-time-zone?key={TIME_API_KEY}&format=json&fields=countryCode,countryName,zoneName,gmtOffset,timestamp,dst").json()
+    data = requests.get(
+        f"https://api.timezonedb.com/v2.1/list-time-zone"
+        f"?key={TIME_API_KEY}"
+        f"&format=json"
+        f"&fields=countryCode,countryName,zoneName,gmtOffset,timestamp,dst",
+    ).json()
 
     for zone in data["zones"]:
         for eachtype in findtype:
             if to_find in zone[eachtype].lower():
-                
-                country_name = zone['countryName']
-                country_zone = zone['zoneName']
-                country_code = zone['countryCode']
+                country_name = zone["countryName"]
+                country_zone = zone["zoneName"]
+                country_code = zone["countryCode"]
 
-                if zone['dst'] == 1:
+                if zone["dst"] == 1:
                     daylight_saving = "Yes"
                 else:
                     daylight_saving = "No"
@@ -30,40 +31,48 @@ def generate_time(to_find: str, findtype: List[str]) -> str:
                 date_fmt = r"%d-%m-%Y"
                 time_fmt = r"%H:%M:%S"
                 day_fmt = r"%A"
-                gmt_offset = zone['gmtOffset']
-                timestamp = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=gmt_offset)
+                gmt_offset = zone["gmtOffset"]
+                timestamp = (
+                    datetime.datetime.now(
+                        datetime.timezone.utc,
+                    )
+                    + datetime.timedelta(seconds=gmt_offset)
+                )
                 current_date = timestamp.strftime(date_fmt)
                 current_time = timestamp.strftime(time_fmt)
                 current_day = timestamp.strftime(day_fmt)
 
                 break
-    
+
     try:
-        result = "<b>🌍Country :</b> <code>{}</code>\n" \
-                 "<b>⏳Zone Name :</b> <code>{}</code>\n" \
-                 "<b>🗺Country Code :</b> <code>{}</code>\n" \
-                 "<b>🌞Daylight saving :</b> <code>{}</code>\n" \
-                 "<b>🏖Day :</b> <code>{}</code>\n" \
-                 "<b>⌚Current Time :</b> <code>{}</code>\n" \
-                 "<b>📆Current Date :</b> <code>{}</code>".format(country_name, country_zone, country_code, daylight_saving, current_day, current_time, current_date)
+        result = (
+            f"<b>Country:</b> <code>{country_name}</code>\n"
+            f"<b>Zone Name:</b> <code>{country_zone}</code>\n"
+            f"<b>Country Code:</b> <code>{country_code}</code>\n"
+            f"<b>Daylight saving:</b> <code>{daylight_saving}</code>\n"
+            f"<b>Day:</b> <code>{current_day}</code>\n"
+            f"<b>Current Time:</b> <code>{current_time}</code>\n"
+            f"<b>Current Date:</b> <code>{current_date}</code>\n"
+            '<b>Timezones:</b> <a href="https://en.wikipedia.org/wiki/List_of_tz_database_time_zones">List here</a>'
+        )
     except:
         result = None
 
     return result
 
 
-@run_async
-def gettime(bot: Bot, update: Update):
-
+def gettime(update: Update, context: CallbackContext):
     message = update.effective_message
-    
+
     try:
         query = message.text.strip().split(" ", 1)[1]
     except:
         message.reply_text("Provide a country name/abbreviation/timezone to find.")
         return
-        
-    send_message = message.reply_text(f"🔎Finding timezone info for <b>{query}</b>", parse_mode=ParseMode.HTML)
+    send_message = message.reply_text(
+        f"Finding timezone info for <b>{query}</b>",
+        parse_mode=ParseMode.HTML,
+    )
 
     query_timezone = query.lower()
     if len(query_timezone) == 2:
@@ -72,21 +81,25 @@ def gettime(bot: Bot, update: Update):
         result = generate_time(query_timezone, ["zoneName", "countryName"])
 
     if not result:
-        send_message.edit_text(f"Timezone info not available for <b>{query}</b>", parse_mode=ParseMode.HTML)
+        send_message.edit_text(
+            f"Timezone info not available for <b>{query}</b>\n"
+            '<b>All Timezones:</b> <a href="https://en.wikipedia.org/wiki/List_of_tz_database_time_zones">List here</a>',
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=True,
+        )
         return
 
-    send_message.edit_text(result, parse_mode=ParseMode.HTML)
+    send_message.edit_text(
+        result,
+        parse_mode=ParseMode.HTML,
+        disable_web_page_preview=True,
+    )
 
 
-__help__ = """
- - /time <query> : Gives information about a timezone.
-Available queries : Country Code/Country Name/Timezone Name
-"""
-
-TIME_HANDLER = DisableAbleCommandHandler("time", gettime)
+TIME_HANDLER = DisableAbleCommandHandler("time", gettime, run_async=True)
 
 dispatcher.add_handler(TIME_HANDLER)
 
-__mod_name__ = "TIME"
+__mod_name__ = "Time"
 __command_list__ = ["time"]
 __handlers__ = [TIME_HANDLER]
